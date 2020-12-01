07359765e7454bb9031ebb6d0275e00b0aab7105
Maarten P. Scholl <scholl.maarten@gmail.com>
2020-12-01 23:12:53 +0000

    add publicly traded company

---
+++ traded_company.cpp
@@ -0,0 +1,170 @@
+/// \file   traded_company.cpp
+///
+/// \brief  
+///
+/// \authors    Maarten P. Scholl
+/// \date       2020-01-20
+/// \copyright  Copyright 2017-2020 The Institute for New Economic Thinking, Oxford Martin School, University of Oxford
+///
+///             Licensed under the Apache License, Version 2.0 (the "License");
+///             you may not use this file except in compliance with the License.
+///             You may obtain a copy of the License at
+///
+///                 http://www.apache.org/licenses/LICENSE-2.0
+///
+///             Unless required by applicable law or agreed to in writing, software
+///             distributed under the License is distributed on an "AS IS" BASIS,
+///             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///             See the License for the specific language governing permissions and
+///             limitations under the License.
+///
+///             You may obtain instructions to fulfill the attribution requirements in CITATION.cff
+///
+#include "traded_company.hpp"
+using esl::economics::finance::dividend_announcement_message;
+
+#include <random>
+
+using namespace esl;
+using namespace esl::economics;
+using namespace esl::economics::finance;
+using namespace esl::law;
+
+using esl::economics::accounting::inventory_filter;
+using esl::economics::currencies::USD;
+using esl::interaction::transfer;
+using esl::economics::accounting::inventory_filter;
+
+///
+/// \param i
+/// \param j
+traded_company::traded_company( const identity<traded_company> &i
+                              , const jurisdiction &j
+                              , uint64_t sample
+                              , time_point end_point
+                              )
+: agent(i)
+, owner<cash>(i)
+, owner<stock>(i)
+, identifiable_as<traded_company>()
+, company(i,j)
+, dividend_per_share(0.01)
+{
+    create_output<double>("dividend_per_share");
+
+    std::seed_seq seed_ {
+            std::uint64_t(std::hash<identity<agent>>()(i))
+            , sample + 2
+    };
+
+    std::minstd_rand0 generator_(seed_);
+
+    unsigned int dividend_receiving_shares_ = 0;
+    for(const auto &[s, q] : shares_outstanding) {
+        if(s.dividend) {
+            dividend_receiving_shares_ += q;
+        }
+    }
+
+    constexpr double dt = 1.; // discretization step
+
+    for(size_t t = 0; t < end_point; ++t){
+
+        std::normal_distribution<double> standard_normal_(0,1);
+        auto z = standard_normal_(generator_);
+
+        if(wiener.size() > tau){
+            // zero based index, so subtract another 1 with tau
+            z = (1.0 - omega) * z + omega * wiener[wiener.size() - 1 - tau];
+        }
+        wiener.push_back(z);
+
+        // reflect so that dividend is always positive
+        dividend_per_share = abs(dividend_per_share
+                                 + mu * dividend_per_share * dt
+                                 + sigma * dividend_per_share * wiener.back());
+
+        dividends_per_share.push_back(dividend_per_share);
+    }
+}
+
+///
+/// \param interval
+/// \param s
+/// \return
+time_point traded_company::act(time_interval interval, std::seed_seq &seed)
+{
+    auto possible_policy_ = upcoming_dividend(interval, seed);
+    auto next_event_      = interval.lower + 1;
+
+    if(!possible_policy_.has_value()) {
+        return next_event_;
+    }
+
+    auto policy_ = possible_policy_.value();
+
+    if(interval.lower >= policy_.announcement_date) {
+        if(last_announced_ < policy_.announcement_date) {
+            last_announced_ = policy_.announcement_date;
+
+            for(const auto &s : unique_shareholders()) {
+                // notifies agents of upcoming dividend
+                this->template create_message<dividend_announcement_message>(
+                        s, interval.lower, this->identifier, s, policy_);
+            }
+        }
+    }else{
+        next_event_ = std::min<time_point>(
+                policy_.announcement_date, next_event_);
+    }
+
+    if(interval.lower >= policy_.payable_date) {
+        if(last_payment_ < policy_.announcement_date) {
+            last_payment_ = policy_.announcement_date;
+        }
+    }else{
+        next_event_ = std::min<time_point>(
+                policy_.announcement_date, next_event_);
+    }
+    return next_event_;
+}
+
+///
+/// \param interval
+/// \return
+std::optional<dividend_policy> traded_company::upcoming_dividend(time_interval interval, std::seed_seq &seed)
+{
+    std::minstd_rand0 generator_(seed);
+
+    unsigned int dividend_receiving_shares_ = 0;
+    for(const auto &[s, q]: shares_outstanding) {
+        if(s.dividend) {
+            dividend_receiving_shares_ += q;
+        }
+    }
+
+    outputs["dividend_per_share"]->write(std::make_tuple(interval.lower,
+                                                         dividends_per_share[interval.lower]));
+
+    auto unappropriated_profit_ = cash(USD).price(
+        dividends_per_share[interval.lower] * dividend_receiving_shares_);
+
+    LOG(trace) << "company " << identifier << " pays out "
+               << unappropriated_profit_
+               << " in profits, distributed over "
+               << dividend_receiving_shares_ << " shares"
+               << std::endl;
+
+    auto dividends_ = compute_dividend_per_share(unappropriated_profit_);
+
+    dividend_policy policy_(
+            interval.lower
+            , interval.lower
+            , interval
+            , interval.lower
+            , primary_jurisdiction.tender
+            , dividends_
+            );
+
+    return policy_;
+}
---
+++ traded_company.hpp
@@ -0,0 +1,162 @@
+/// \file   traded_company.hpp
+///
+/// \brief  In the market ecology model traded companies generate fictitious revenues.
+///
+/// \authors    Maarten
+/// \date       2020-01-20
+/// \copyright  Copyright 2017-2020 The Institute for New Economic Thinking, Oxford Martin School, University of Oxford
+///
+///             Licensed under the Apache License, Version 2.0 (the "License");
+///             you may not use this file except in compliance with the License.
+///             You may obtain a copy of the License at
+///
+///                 http://www.apache.org/licenses/LICENSE-2.0
+///
+///             Unless required by applicable law or agreed to in writing, software
+///             distributed under the License is distributed on an "AS IS" BASIS,
+///             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///             See the License for the specific language governing permissions and
+///             limitations under the License.
+///
+///             You may obtain instructions to fulfill the attribution requirements in CITATION.cff
+///
+#ifndef ME_TRADED_COMPANY_HPP
+#define ME_TRADED_COMPANY_HPP
+
+#include <esl/economics/finance/company.hpp>
+using esl::economics::company;
+using esl::law::jurisdiction;
+using esl::simulation::time_point;
+using esl::simulation::time_interval;
+using esl::economics::finance::dividend_policy;
+using esl::economics::price;
+
+///
+/// \brief  A traded company issues shares and pays dividends
+///
+class traded_company
+: public company
+, public esl::identifiable_as<traded_company>
+{
+public:
+    ///
+    /// \param i    company identity
+    /// \param j    jurisdiction (for accounting)
+    /// \param sample   seed for random dividend process
+    traded_company( const esl::identity<traded_company> &i
+                  , const jurisdiction &j
+                  , uint64_t sample = 0
+                  , time_point end_point = 201 * 252
+                  );
+
+    ///
+    /// \brief  Generates new profits and decides on next dividend
+    ///
+    /// \param interval
+    /// \param s
+    /// \return
+    time_point act(time_interval interval, std::seed_seq &s) override;
+
+    ///
+    /// \brief  Estimates the first upcoming dividend payment
+    ///
+    /// \param interval
+    /// \param seed
+    /// \return
+    std::optional<dividend_policy> upcoming_dividend( time_interval interval
+                                                    , std::seed_seq &seed
+                                                    ) override;
+
+    ///
+    /// \brief  Number of days in a year. Used to annualise economic quantities
+    ///
+    constexpr static size_t day_count  = 252;
+
+    ///
+    /// \brief  2% growth rate annually
+    ///
+    double mu = pow(1.02, 1./day_count) - 1;
+
+    ///
+    /// \brief  10% volatility annually
+    ///
+    double sigma = 0.10 / sqrt(double(day_count));
+
+    ///
+    /// \brief  autocorrelation lag
+    ///
+    constexpr static size_t tau        = 1;
+
+    ///
+    /// \brief  autocorrelation factor
+    ///
+    constexpr static double omega      = 0.1;
+
+    ///
+    /// \brief  Wiener process realization
+    ///
+    std::vector<double> wiener;
+
+    ///
+    /// \brief  Raw dividends per share
+    /// TODO: convert to price instead of double
+    ///
+    std::vector<double> dividends_per_share;
+
+    ///
+    /// \brief  current dividend per share
+    /// TODO: convert to price instead of double
+    ///
+    double dividend_per_share;
+
+    ///
+    /// \brief  company income
+    ///
+    [[nodiscard]] esl::economics::price income() const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+
+    ///
+    /// \brief  company book value
+    ///
+    [[nodiscard]] esl::economics::price book_value() const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+
+    ///
+    /// \brief  debt
+    ///
+    [[nodiscard]] esl::economics::price debt() const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+
+    ///
+    /// \brief sales for current time
+    /// \return
+    [[nodiscard]] esl::economics::price sales() const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+
+    ///
+    /// \brief costs for current time
+    /// \return
+    [[nodiscard]] esl::economics::price costs() const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+
+    ///
+    /// \brief earnings at current time
+    /// \return
+    [[nodiscard]] esl::economics::price historic_earnings(/*time span*/) const
+    {
+        return esl::economics::price(0, this->primary_jurisdiction.tender);
+    }
+};
+
+
+#endif //ME_TRADED_COMPANY_HPP
