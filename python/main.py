a0bbf2f7f33a765574e66edf165ffe11d0195afa
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-05-04 21:41:09 +0200

    enable Python model overrides

--- python/main.py
+++ python/main.py
@@ -3,22 +3,24 @@ import esl
 class SingleExperiment(esl.simulation.model):
     def __init__(self, environment, parameters):
         super().__init__(environment, parameters)
-
         self.economy = esl.simulation.world()
 
+
+    def clear_market(initial_prices, excess_demand_functions):
+        model = esl.economics.markets.walras.excess_demand_model(initial_prices)
+        model.excess_demand_functions = excess_demand_functions
+        return model.compute_clearing_quotes()
+
+    def step(self, time_interval):
+        print("step")
+        pass
 
 
 
 if __name__ == "__main__":
-    e = esl.computation.environment()
-    ps = esl.simulation.parameter.parametrization()
-    ks = ps.values.keys()
-    print(ks[0])
-    m = SingleExperiment(e, ps)
+    environment = esl.computation.environment()
+    parameters = esl.simulation.parameter.parametrization()
 
-    e.run(m)
+    model = SingleExperiment(environment, parameters)
 
-def clear_market(initial_prices, excess_demand_functions):
-    model = excess_demand_model(initial_prices)
-    model.excess_demand_functions = excess_demand_functions
-    return model.compute_clearing_quotes()
+    environment.run(model)
