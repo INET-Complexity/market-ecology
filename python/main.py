3333526bf71d4afd758af39e5e1470375f381f4a
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-05-10 17:08:51 +0200

    simplify conversion of identity class

--- python/main.py
+++ python/main.py
@@ -1,5 +1,22 @@
 import esl
 
+
+
+class Fund(esl.economics.company):
+    def __init__(self, identifier, jurisdiction = esl.law.jurisdictions.US):
+        super().__init__(identifier, jurisdiction)
+        pass
+
+
+class NoiseTrader(Fund):
+
+    def __init__(self, identifier):
+        super().__init__(identifier)
+
+    def act(self, esl.simulation.time_interval si, esl.simulation.seed):
+
+
+
 class SingleExperiment(esl.simulation.model):
     def __init__(self, environment, parameters):
         super().__init__(environment, parameters)
@@ -17,18 +34,31 @@ class SingleExperiment(esl.simulation.model):
         :param time_interval:
         :return:
         """
-        print(f"Here we do model-specific logic {time_interval}")
+        # Here we do model-specific logic
+
+        print(f"step {time_interval}")
         return time_interval.upper
 
 
 
 if __name__ == "__main__":
+    nt = NoiseTrader(esl.simulation.identity([1,2]))
+
+
+
     environment = esl.computation.environment()
     parameters = esl.simulation.parameter.parametrization()
 
     model = SingleExperiment(environment, parameters)
+    print(model)
+    print(environment)
+
+
+
 
     se = esl.simulation.time_interval(1,10)
+
+    print(  environment.run  )
 
     a = model.step(se)
     print(model.start)
