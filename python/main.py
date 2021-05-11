9734ce1f3ec4f1a913c185bc9a3a88ffe5df57f2
Maarten P. Scholl <scholl.maarten@gmail.com>
2021-05-11 18:42:15 +0200

    start writing Python experiment

--- python/main.py
+++ python/main.py
@@ -6,20 +6,11 @@ from esl.simulation import identity, model, parameter, time_point, time_interval
 from esl.economics.markets.walras import excess_demand_model, differentiable_order_message
 
 
-class DebugEnv(environment):
-    def __init__(self):
-        super().__init__()
-        pass
-
-    def run(self, sim):
-        print("LOL!")
-        print(super().run)
-
-
 class Fund(esl.economics.company):
     def __init__(self, identifier, jurisdiction = esl.law.jurisdictions.US):
         super().__init__(identifier, jurisdiction)
         pass
+
 
 class NoiseTrader(Fund):
 
@@ -27,7 +18,7 @@ class NoiseTrader(Fund):
         super().__init__(identifier)
 
     def act(self, si: esl.simulation.time_interval, s: esl.seed):
-        print("NT act")
+        print("NT acts")
         pass
 
     class NoiseTraderOrder(differentiable_order_message):
@@ -48,9 +39,35 @@ class NoiseTrader(Fund):
             return ed
 
 
+class ValueInvestor(Fund):
+
+    def __init__(self, identifier):
+        super().__init__(identifier)
+
+    def act(self, si: esl.simulation.time_interval, s: esl.seed):
+        print("VI acts")
+        pass
+
+    class ValueInvestorOrder(differentiable_order_message):
+
+        def __init__(self, sender, recipient, sent, received):
+            super().__init__(sender, recipient, sent, received)
+            self.sender = sender
+
+        def excess_demand(self, quotes):
+            """
+
+            :param quotes: A dict with property_identifier keys and pairs (quote, variable)
+            :return:
+            """
+            print(f"The Walrasian price setter suggests the following prices: {quotes}")
+            ed = {k: ((i+3.) - (float(v[0]) * v[1])) for i,  (k, v) in enumerate(quotes.items())}
+            print(f"Agent {self.sender}'s excess demand at these prices is: {ed}")
+            return ed
 
 
-class SingleExperiment(esl.simulation.model):
+
+class Experiment1(esl.simulation.model):
     def __init__(self, environment, parameters):
         super().__init__(environment, parameters)
         #self.economy = esl.simulation.world()
@@ -68,26 +85,24 @@ class SingleExperiment(esl.simulation.model):
         """
         # Here we do model-specific logic
 
-        print(f"step {time_interval}")
+        print(f"{type(self).__name__} time step {time_interval}")
         return time_interval.upper
 
 
 
 if __name__ == "__main__":
-
-
-    e =  DebugEnv()
+    e =  environment()
     parameters = parameter.parametrization()
 
-    m = SingleExperiment(e, parameters)
+    m = Experiment1(e, parameters)
 
-    print(e.run)
 
-    nt = NoiseTrader(identity([1,2]))
+    nt = NoiseTrader(identity([1]))
+    vi = ValueInvestor(identity([2]))
 
-    se = time_interval(1,10)
+    simulation_timespan = time_interval(1, 252*10)
 
-    e.run(m)
+    m.step(simulation_timespan)
 
 
 
